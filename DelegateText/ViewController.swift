9ea12f6cce3271f16c664c5f205f7db2097591dc
kauemendes <kaue.mendes@gmail.com>
2015-05-10 18:31:04 -0300

    Como trabalhar com Delegates: Exemplos

--- DelegateText/ViewController.swift
+++ DelegateText/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
+    @IBOutlet weak var txtField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +19,15 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        
+        txtField.resignFirstResponder();
+        
+        return false;
+        
     }
 
 
